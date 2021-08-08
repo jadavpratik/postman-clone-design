3ffc9c2866806dd146b8db5c5f1b80bdcdfd5802
Zrough <54739706+jadavpratik@users.noreply.github.com>
2021-08-08 10:41:32 +0530

    'design'

--- What.js
+++ What.js
@@ -1,8 +1,3 @@
-FORM, JSON,
-ALERT for SUCCESS as well as ERROR, WARNING,
-HEADER contenct type and much more,
-RESPONSE-PRINT WITH OBJECT AS WELL AS JSON,
-WITH STATUS CODE,
 
 cookie issue and also first see the post man how works
 CRUD
